65d88f0a617138ad6567d1650fca3a32cd4ff029
tristan <mousy93@hotmail.com>
2022-05-22 19:09:35 +0800

    updated post model to contain author_id

--- models/post.js
+++ models/post.js
@@ -21,15 +21,16 @@ Post.init(
     },
     datePosted: {
       type: DataTypes.DATE,
-      allowNull: false,
+      allowNull: true,
       defaultValue: DataTypes.NOW,
     },
-    user_id: {
+    author_id: {
       type: DataTypes.INTEGER,
-      references: {
-        model: "user",
-        key: "id",
-      },
+      allowNull: false,
+      // references: {
+      //   model: "user",
+      //   key: "id",
+      // },
     },
   },
   {
